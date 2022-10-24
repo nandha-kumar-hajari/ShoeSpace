9200bd1e1dfae5a444c0443b6eb9312b04290002
Nandha Kumar <nandhakumarh@mirakitech.com>
2022-10-24 12:40:44 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const App=() =>{
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+
+}
+
+export default App
